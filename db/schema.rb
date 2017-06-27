0ffc052dee7dfbaf325390392133b212e76e874e
cmac1223 <chrismac539@gmail.com>
2017-06-27 14:22:47 -0400

    removed user table because of devise fuckup

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627171627) do
+ActiveRecord::Schema.define(version: 20170627182203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
