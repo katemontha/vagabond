3585dcf2a2140ed3881d92c4d36b3be82bfcf2f7
rolita1007 <rolita1007@gmail.com>
2017-06-28 16:17:01 -0400

    resolved merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :users
 
   resources :cities do
     resources :posts
   end
-<<<<<<< HEAD
-=======
-  
->>>>>>> d940447a5955bc4c56002744c628c33fc1e72625
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
