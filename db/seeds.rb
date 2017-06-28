9e896bedaa4e941c79f9fff7ab8493d7f4c058d1
katemontha <katemontha@gmail.com>
2017-06-28 17:33:55 -0400

    adding links and button functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.destroy_all
 Post.destroy_all
+User.destroy_all
 City.destroy_all
 
 user1 = User.create(name: 'Yeni', username: 'Rolita', email: 'yeni@gmail.com', password: 'nosenose', hometown: 'Colombia')
 city1 = City.create(name: 'Atlanta')
+city2 = City.create(name: 'London')
+city3 = City.create(name: 'San Fransico')
 posts = Post.create(content: 'Capital of Georgia', user_id: user1.id, city_id: city1.id)
 
 
