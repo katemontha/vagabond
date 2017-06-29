fe3a2a6938fbbad7a05bc8f5c261e2c023f2c2ec
katemontha <katemontha@gmail.com>
2017-06-29 14:32:14 -0400

    pushing for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,18 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    # # devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :picture, :name, :hometown, :password, :password_confirmation, :remember_me])
+
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+
+
+  #    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :picture, :name, :hometown, :password, :password_confirmation, :remember_me])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :username, :email, :password, :remember_me])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :picture, :name, :hometown, :password, :password_confirmation, :current_password])
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      puts user_params
-      user_params.permit({roles: []}, :email, :password, :password_confirmation, :username, :hometown, :picture, :name)
-    end
+    # devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    #   puts user_params
+    #   puts "HEY I'm DOING SOMETHING"
+    #   user_params.permit({roles: []}, :email, :password, :password_confirmation, :username, :hometown, :picture, :name)
+    # end
     
   end   
 
